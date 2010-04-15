995f9929899ef825ac76d620d336da5a5b5cc4a6
Uwe L. Korn <uwelk@xhochy.org>
2010-04-15 17:45:37 +0200

    Initial stuff to get Sinatra working.

--- app.rb
+++ app.rb
@@ -1,32 +1,29 @@
 # This file contains your application, it requires dependencies and necessary
 # parts of the application.
 #
-# It will be required from either `config.ru` or `start.rb`
-
-require 'rubygems'
-require 'ramaze'
+# It will be required from `config.ru`
 
 # Include needed gems
+require 'rubygems'
+require 'sinatra/base'
 require 'scrobbler'
 require 'dm-core'
 
 # Include local configuration
 require 'local-config'
 
-# Make sure that Ramaze knows where you are
-Ramaze.options.roots = [__DIR__]
-Ramaze.options.adapter.adapter = :mongrel
-
 # Load the Classification backend
-require __DIR__('classification/resolver')
+require File.join(File.dirname(__FILE__), 'lib', 'classification', 'resolver')
 $resolver = Classification::Resolver.new(:source => :files, :files => 
   File.join(File.dirname(__FILE__), 'classification', 'countries', 
     '*.yml'))
 
-# Initialize controllers and models
-require __DIR__('model/init')
-require __DIR__('controller/init')
-
 DataMapper.auto_migrate!
 
+
+class LastFMNations < Sinatra::Base
+  get '/' do
+    'Hello World!'
+  end
+end
 
