4ca27e82fe7024b1fba6e9c286925a4fe3b3d46a
Uwe L. Korn <uwelk@xhochy.org>
2010-04-18 10:03:08 +0200

    Extracted controller code into separate files.

--- app.rb
+++ app.rb
@@ -23,56 +23,13 @@ DataMapper.auto_migrate!
 
 # Load the Controllers
 class LastFMNations < Sinatra::Base
+
   get '/' do
     'This is the Nationality Statistics Server.'
   end
-  
-  get '/artist/classify/:name.:format' do
-    @name = params[:name]
-    @title = @name 
-    @country = $resolver.by_scrobbler_tags(Scrobbler::Artist.new(:name => @name).top_tags)
-    
-    # Render
-    case params[:format]
-      when 'xml'
-        @content = haml :artist_classify_xml
-        return haml :default_xml
-      when 'xhtml'
-        @content = haml :artist_classify_xhtml
-        return haml :default_xhtml
-      else
-        halt 404
-    end
-  end
-  
-  get '/artist/info/:name.:format' do
-    @name = params[:name]
-    artist = Scrobbler::Artist.new(:name => @name)
-    @title = @name 
-    top_tags = artist.top_tags
-    @countries = $resolver.by_scrobbler_tags_ex(top_tags)
-    @recognized_tags = {}
-    @unrecognized_tags = {}
-    top_tags.each do |tag|
-      if $resolver.has_tag?(tag.name)
-        @recognized_tags[tag.name] = tag.count  
-      else
-        @unrecognized_tags[tag.name] = tag.count
-      end
-    end
-
-    # Render
-    case params[:format]
-      when 'xml'
-        @content = haml :artist_info_xml
-        return haml :default_xml
-      when 'xhtml'
-        @content = haml :artist_info_xhtml
-        return haml :default_xhtml
-      else
-        halt 404
-    end
-  end
 
 end
+
+require 'controller/library'
+require 'controller/artist'
 
---
+++ controller/artist.rb
@@ -0,0 +1,51 @@
+class LastFMNations
+  
+  get '/artist/classify/:name.:format' do
+    @name = params[:name]
+    @title = @name 
+    @country = $resolver.by_scrobbler_tags(Scrobbler::Artist.new(:name => @name).top_tags)
+    
+    # Render
+    case params[:format]
+      when 'xml'
+        @content = haml :artist_classify_xml
+        return haml :default_xml
+      when 'xhtml'
+        @content = haml :artist_classify_xhtml
+        return haml :default_xhtml
+      else
+        halt 404
+    end
+  end
+  
+  get '/artist/info/:name.:format' do
+    @name = params[:name]
+   artist = Scrobbler::Artist.new(:name => @name)
+    @title = @name 
+    top_tags = artist.top_tags
+    @countries = $resolver.by_scrobbler_tags_ex(top_tags)
+    @recognized_tags = {}
+    @unrecognized_tags = {}
+    top_tags.each do |tag|
+      if $resolver.has_tag?(tag.name)
+        @recognized_tags[tag.name] = tag.count  
+      else
+        @unrecognized_tags[tag.name] = tag.count
+      end
+    end
+
+    # Render
+    case params[:format]
+      when 'xml'
+        @content = haml :artist_info_xml
+        return haml :default_xml
+      when 'xhtml'
+        @content = haml :artist_info_xhtml
+        return haml :default_xhtml
+      else
+        halt 404
+    end
+  end
+
+end
+
