f3307f54b815b739482983c7f19e9538b4e92fa4
Uwe L. Korn <uwelk@xhochy.org>
2010-04-18 09:34:31 +0200

    Fixed /artist/* views

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ require 'local-config'
 # Load the Classification backend
 require File.join(File.dirname(__FILE__), 'lib', 'classification', 'resolver')
 $resolver = Classification::Resolver.new(:source => :files, :files => 
-  File.join(File.dirname(__FILE__), 'classification', 'countries', 
+  File.join(File.dirname(__FILE__), 'lib', 'classification', 'countries', 
     '*.yml'))
 
 DataMapper.auto_migrate!
@@ -31,11 +31,44 @@ class LastFMNations < Sinatra::Base
     @name = params[:name]
     @title = @name 
     @country = $resolver.by_scrobbler_tags(Scrobbler::Artist.new(:name => @name).top_tags)
+    
+    # Render
     case params[:format]
       when 'xml'
-        return haml :artist_classify_xml
+        @content = haml :artist_classify_xml
+        return haml :default_xml
       when 'xhtml'
-        return haml :artist_classify_xhtml
+        @content = haml :artist_classify_xhtml
+        return haml :default_xhtml
+      else
+        halt 404
+    end
+  end
+  
+  get '/artist/info/:name.:format' do
+    @name = params[:name]
+    artist = Scrobbler::Artist.new(:name => @name)
+    @title = @name 
+    top_tags = artist.top_tags
+    @countries = $resolver.by_scrobbler_tags_ex(top_tags)
+    @recognized_tags = {}
+    @unrecognized_tags = {}
+    top_tags.each do |tag|
+      if $resolver.has_tag?(tag.name)
+        @recognized_tags[tag.name] = tag.count  
+      else
+        @unrecognized_tags[tag.name] = tag.count
+      end
+    end
+
+    # Render
+    case params[:format]
+      when 'xml'
+        @content = haml :artist_info_xml
+        return haml :default_xml
+      when 'xhtml'
+        @content = haml :artist_info_xhtml
+        return haml :default_xhtml
       else
         halt 404
     end
